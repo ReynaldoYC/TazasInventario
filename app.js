b79a5aac2ce3979aaa3103fc673cb30d4e1fa76c
ReynaldoYC <yacila1717@gmail.com>
2020-08-29 06:28:44 -0500

    asd

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const ws = new WebSocket('wss://app.netlify.com/sites/inventariotazas/');
-ws.onopen = () => {
-    console.log('conectado');
-};
-ws.onerror = (e) => {
-    console.log(e);
-};
+// const ws = new WebSocket('wss://app.netlify.com/sites/inventariotazas/');
+// ws.onopen = () => {
+//     console.log('conectado');
+// };
+// ws.onerror = (e) => {
+//     console.log(e);
+// };
